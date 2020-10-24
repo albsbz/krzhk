4b809fa84f6436f29e73afbc21183339f2e8988e
albsbz <best.shop.bot@gmail.com>
2020-10-24 18:48:45 +0300

    gmap

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,7 +2,9 @@ export default {
   // Disable server-side rendering (https://go.nuxtjs.dev/ssr-mode)
   ssr: true,
   target:'static',
-
+  env: {
+    GMAP_API_KEY: 'AIzaSyA5X3nMtXIcE__bKV8sSB3xqzxzxC_Urm0'
+  },
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
     title: 'krzhk',
@@ -21,7 +23,7 @@ export default {
   ],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
-  plugins: [{ src: '~plugins/vue-carousel', ssr: false }],
+  plugins: [{ src: '~plugins/vue-carousel', ssr: false }, { src: '~plugins/gmap', ssr: false }],
 
   // Auto import components (https://go.nuxtjs.dev/config-components)
   components: true,
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,36 @@
 <template>
   <div>
     index page
+    <GmapMap
+      :center="{ lat: 47.840476, lng: 35.137578 }"
+      :zoom="10"
+      map-type-id="roadmap"
+      class="box"
+      style="width: 500px; height: 300px"
+    >
+      <GmapMarker
+        v-for="(m, index) in markers"
+        :key="index"
+        :position="m.position"
+        :clickable="false"
+        :draggable="false"
+      />
+    </GmapMap>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      markers: [
+        {
+          position: { lat: 47.841168, lng: 35.122701 }
+        }
+      ]
+    };
+  }
+};
 </script>
 
 <style></style>
