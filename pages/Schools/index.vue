b21e745a618785469ffdd27e9f9698ab3785e75d
albsbz <best.shop.bot@gmail.com>
2020-11-01 21:50:17 +0200

    schooks aside component

--- pages/Schools/index.vue
+++ pages/Schools/index.vue
@@ -18,41 +18,7 @@
         />
       </div>
 
-      <aside class="sidebar box">
-        <h6>Фильтр</h6>
-        <div class="field">
-          <label class="label">Тэги</label>
-          <div class="control">
-            <div class="select">
-              <select v-model="selectedTag" @change="changeFilter">
-                <option v-for="tag of tags" :key="tag" :value="tag">{{
-                  tag
-                }}</option>
-              </select>
-            </div>
-          </div>
-        </div>
-
-        <div class="field">
-          <label class="label">Район</label>
-          <div class="control">
-            <div class="select">
-              <select v-model="selectedDistrict" @change="changeFilter">
-                <option
-                  v-for="district of districts"
-                  :key="district"
-                  :value="district"
-                >
-                  {{ district }}</option
-                >
-              </select>
-            </div>
-          </div>
-        </div>
-        <button class="button is-small" @click="clearAllFilters">
-          Сбросить фильтр
-        </button>
-      </aside>
+      <appSidebar class="sidebar" />
       <nav class="pagination" role="navigation" aria-label="pagination">
         <a class="pagination-previous" @click.prevent="previousPage">Назад</a>
         <a class="pagination-link">{{ $store.state.schools.page + 1 }}</a>
@@ -64,10 +30,17 @@
 
 <script>
 import ShortArticle from "@/components/school/ShortArticle.vue";
+import appSidebar from "@/components/Sidebar.vue";
 
 export default {
   components: {
-    ShortArticle
+    ShortArticle,
+    appSidebar
+  },
+  computed: {
+    schools() {
+      return this.$store.getters["schools/onePage"];
+    }
   },
   // async fetch() {
   //   console.log("fetch");
@@ -81,35 +54,6 @@ export default {
   //   // const schools = context.store.getters["schools/onePage"];
   //   // return { schools };
   // },
-
-  data() {
-    return {
-      tags: this.$store.getters["schools/tags"],
-      districts: this.$store.getters["schools/districts"]
-    };
-  },
-  computed: {
-    selectedTag: {
-      get() {
-        return this.$store.state.schools.filterTag;
-      },
-      set(value) {
-        this.$store.commit("schools/setFilterTag", value);
-      }
-    },
-
-    selectedDistrict: {
-      get() {
-        return this.$store.state.schools.filterDistrict;
-      },
-      set(value) {
-        this.$store.commit("schools/setFilterDistrict", value);
-      }
-    },
-    schools() {
-      return this.$store.getters["schools/onePage"];
-    }
-  },
 
   methods: {
     nextPage() {
@@ -132,14 +76,6 @@ export default {
         // this.schools = this.$store.getters["schools/onePage"];
         window.scrollTo(0, top);
       }
-    },
-    changeFilter() {
-      this.$store.commit("schools/zeroPage");
-    },
-    clearAllFilters() {
-      this.$store.commit("schools/setFilterTag", "Все");
-      this.$store.commit("schools/setFilterDistrict", "Все");
-      this.changeFilter();
     }
   }
 };
