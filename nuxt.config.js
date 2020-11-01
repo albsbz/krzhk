b35eaf71dd51b51e802bf8bbff21a54a8555248d
albsbz <best.shop.bot@gmail.com>
2020-11-01 13:16:52 +0200

    infowindow

--- nuxt.config.js
+++ nuxt.config.js
@@ -29,7 +29,7 @@ export default {
   ],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
-  plugins: [{ src: '~plugins/vue-carousel', ssr: false }, { src: '~plugins/gmap', ssr: false }],
+  plugins: [{ src: '~plugins/vue-carousel', ssr: false }, { src: '~plugins/gmap', ssr: true }],
 
   // Auto import components (https://go.nuxtjs.dev/config-components)
   components: true,
--- pages/index.vue
+++ pages/index.vue
@@ -1,67 +1,62 @@
 <template>
-  <div>
-    index page
+  <div class="container">
+    <h1 class="title is-large">Детские кружки в г. Запорожье</h1>
+    <!-- <div class="dumb"></div> -->
     <GmapMap
       :center="{ lat: 47.840476, lng: 35.137578 }"
       :zoom="10"
       map-type-id="roadmap"
-      class="box"
+      class="box map"
       style="width: 500px; height: 300px"
     >
-      <GmapInfoWindow
-        :options="infoOptions"
-        :position="infoWindowPos"
-        :opened="infoWinOpen"
-        @closeclick="infoWinOpen = false"
-      >
-        <div class="card">
-          <div class="card-image">
-            <figure class="image is-4by3">
-              <img
-                src="https://bulma.io/images/placeholders/96x96.png"
-                alt="Placeholder image"
-              />
-            </figure>
+      <GmapInfoWindow :position="infoWindowPos" :opened="infoWinOpen">
+        <article class="message is-link">
+          <div class="message-header">
+            <NLink :to="'schools/' + infoOptions.content"
+              ><p>{{ infoOptions.content }}</p></NLink
+            >
+            <button
+              class="delete"
+              aria-label="delete"
+              @click="infoWinOpen = false"
+            ></button>
           </div>
-          <div class="card-content">
-            <div class="media">
-              <div class="media-content">
-                <p class="title is-4">${marker.name}</p>
-              </div>
-            </div>
-            <div class="content">
-              ${marker.description}
-              <br />
-              <time datetime="2016-1-1">${marker.date_build}</time>
-            </div>
+          <div class="message-body">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            <strong>Pellentesque risus mi</strong>, tempus quis placerat ut,
+            porta nec nulla. Vestibulum rhoncus ac ex sit amet fringilla. Nullam
+            gravida purus diam, et dictum <a>felis venenatis</a> efficitur.
+            Aenean ac <em>eleifend lacus</em>, in mollis lectus. Donec sodales,
+            arcu et sollicitudin porttitor, tortor urna tempor ligula, id
+            porttitor mi magna a neque. Donec dui urna, vehicula et sem eget,
+            facilisis sodales sem.
           </div>
-        </div>
+        </article>
       </GmapInfoWindow>
-      <!-- <GmapCluster> -->
-      <GmapMarker
-        v-for="(m, index) in markers"
-        :key="index"
-        :position="m.position"
-        :clickable="true"
-        :draggable="false"
-        @click="toggleInfoWindow(m, index)"
-      />
-      <!-- </GmapCluster> -->
+      <GmapCluster>
+        <GmapMarker
+          v-for="(m, index) in markers"
+          :key="index"
+          :position="m.position"
+          :clickable="true"
+          :draggable="false"
+          @click="toggleInfoWindow(m, index)"
+        />
+      </GmapCluster>
     </GmapMap>
   </div>
 </template>
 
 <script>
 import { gmapApi } from "vue2-google-maps";
-// import GmapCluster from "vue2-google-maps/dist/components/cluster";
+import GmapCluster from "vue2-google-maps/dist/components/cluster";
 
 export default {
   components: {
-    // GmapCluster
+    GmapCluster
   },
   data() {
     return {
-      // placeId: "ChIJWfhiyzJn3EARuPWsMqOq3WQ",
       geocoder: null,
       infoWindowPos: null,
       infoWinOpen: false,
@@ -94,6 +89,7 @@ export default {
   methods: {
     toggleInfoWindow(marker, idx) {
       this.infoWindowPos = marker.position;
+      // this.infoOptions.content = marker.infoText;
       this.infoOptions.content = marker.infoText;
 
       // check if its the same marker that was selected if yes toggle
@@ -110,4 +106,19 @@ export default {
 };
 </script>
 
-<style></style>
+<style scoped>
+.gm-style-iw-d {
+  overflow: hidden !important;
+}
+.dumb {
+  width: 500px;
+  height: 300px;
+  background-color: aqua;
+}
+.title,
+.map {
+  margin: 0 auto;
+  display: block;
+  text-align: center;
+}
+</style>
